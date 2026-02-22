08a8295ad39880aefefd0dde29fcb5b2923c4320
RemziEfeTastan74 <remziefetastan74@gmail.com>
2026-02-23 00:43:56 +0300

    Back ve Start Game yapıldı

---
+++ Assets/Scripts/GoMainMenu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoMainMenu : MonoBehaviour
+{
+    public void BackMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
