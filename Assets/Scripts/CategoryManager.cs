a3610376d1a17100e88bd4379b9852880a752dd7
RemziEfeTastan74 <remziefetastan74@gmail.com>
2026-03-01 22:55:46 +0300

    GameScene ve paneller eklendi

---
+++ Assets/Scripts/CategoryManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CategoryManager : MonoBehaviour
+{
+    public static string selectedCategory;
+
+    public void SelectCategory(string categoryName)
+    {
+        selectedCategory = categoryName;
+        SceneManager.LoadScene("GameScene");
+    }
+}
--- Assets/Scripts/Managers/HistoryListWrapper.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-[Serializable]
-public class HistoryListWrapper
-{
-    public List<HistoryData> histories;
-}
